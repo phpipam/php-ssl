92189cd3e0388a5cbbbde58e0e1f7057d05d47a8
Miha Petkovšek <miha.petkovsek@telemach.si>
2026-02-26 11:58:14 +0100

    fixes

--- route/zones/zone/host/host-certificate.php
+++ route/zones/zone/host/host-certificate.php
@@ -2,128 +2,111 @@
 
 $icon_cert = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-certificate"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 15m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M13 17.5v4.5l2 -1.5l2 1.5v-4.5" /><path d="M10 19h-5a2 2 0 0 1 -2 -2v-10c0 -1.1 .9 -2 2 -2h14a2 2 0 0 1 2 2v3.5" /></svg>';
 
-?>
+$cards = [
+	[
+		'title'        => _("Current certificate"),
+		'header_class' => '',
+		'card_style'   => '',
+		'is_current'   => true,
+		'cert_parsed'  => $cert_parsed,
+		'status_text'  => $status['text'],
+		'san_list'     => $san_list,
+		'days_class'   => $days_class,
+		'days_valid'   => $days_valid,
+	],
+];
 
-<div class='card'>
-	<div class='card-header'><?php print $icon_cert; ?> <?php print _("Current certificate"); ?></div>
-	<div>
-	<?php if ($cert_parsed === null || empty($cert_parsed)): ?>
-	<table class='table table-borderless table-md table-hover table-td-top table-zones-details'>
-		<tr>
-			<td colspan='2'><span class='text-secondary'><?php print _("No certificate data available."); ?></span></td>
-		</tr>
-	</table>
-	<?php else: ?>
-	<table class='table table-borderless table-md table-hover table-td-top table-zones-details table-condensed'>
+if (!empty($host->c_id_old)) {
+	$cards[] = [
+		'title'        => _("Previous certificate"),
+		'header_class' => 'text-secondary',
+		'card_style'   => 'margin-top:10px;',
+		'is_current'   => false,
+		'cert_parsed'  => $cert_old_parsed,
+		'status_text'  => $cert_old_status['text'],
+		'san_list'     => $cert_old_san_list,
+		'days_class'   => $cert_old_days_class,
+		'days_valid'   => $cert_old_days_valid,
+	];
+}
 
-		<tr>
-			<td class="text-secondary" style='min-width:160px;width:180px;'><?php print _("Status"); ?></td>
-			<td>
-				<?php print ($status['text']); ?>
-			</td>
-		</tr>
+foreach ($cards as $c):
 
-		<tr>
-			<td class="text-secondary"><?php print _("Serial"); ?></td>
-			<td>
-				<a class='btn btn-sm' target="_blank" href='/<?php print $_params['tenant']; ?>/certificates/<?php print $zone->name; ?>/<?php print $cert_parsed['serialNumber']; ?>/'>
-					<?php print htmlspecialchars(chunk_split($cert_parsed['serialNumber'])); ?>
-				</a>
-			</td>
-		</tr>
+	$has_cert    = !($c['cert_parsed'] === null || empty($c['cert_parsed']) || $c['cert_parsed']['serialNumberHex']==="/");
+	$table_class = 'table table-borderless table-md table-hover table-zones-details';
+	if ($c['is_current']) {
+		$table_class .= ' table-td-top';
+		if ($has_cert) { $table_class .= ' table-condensed'; }
+	}
+	?>
+	<div class='card'<?php if ($c['card_style']) { print " style='" . $c['card_style'] . "'"; } ?>>
+		<div class='card-header<?php if ($c['header_class']) { print ' ' . $c['header_class']; } ?>'><?php print $icon_cert; ?> <?php print $c['title']; ?></div>
+		<div>
 
-		<tr>
-			<td class="text-secondary"><?php print _("Common name"); ?></td>
-			<td><?php print htmlspecialchars($cert_parsed['subject']['CN'] ?? '/'); ?></td>
-		</tr>
 
-		<?php if (!empty($san_list)): ?>
-		<tr>
-			<td class="text-secondary"><?php print _("SANs"); ?></td>
-			<td><?php print implode("<br>", array_map('htmlspecialchars', $san_list)); ?></td>
-		</tr>
+		<?php if ($c['is_current'] && !$has_cert): ?>
+		<div class='alert alert-info' style='margin:10px;'>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path><path d="M12 9h.01"></path><path d="M11 12h1v4h1"></path></svg>
+			<?php print _("No certificate found."); ?>
+		</div>
+
+		<?php elseif (!$has_cert && $host->c_id_old!==NULL): ?>
+		<div class='alert alert-info' style='margin:10px;'>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path><path d="M12 9h.01"></path><path d="M11 12h1v4h1"></path></svg>
+			<?php print _("Previous certificate already removed from database."); ?>
+		</div>
+
+		<?php else: ?>
+		<table class='<?php print $table_class; ?>'>
+
+			<tr>
+				<td class="text-secondary" style='min-width:160px;width:180px;'><?php print _("Status"); ?></td>
+				<td>
+					<?php print $c['status_text']; ?>
+				</td>
+			</tr>
+
+			<tr>
+				<td class="text-secondary"><?php print _("Serial"); ?></td>
+				<td>
+					<a class='btn btn-sm' target="_blank" href='/<?php print $_params['tenant']; ?>/certificates/<?php print $zone->name; ?>/<?php print $c['cert_parsed']['serialNumber']; ?>/'>
+						<?php print htmlspecialchars(chunk_split($c['cert_parsed']['serialNumber'])); ?>
+					</a>
+				</td>
+			</tr>
+
+			<tr>
+				<td class="text-secondary"><?php print _("Common name"); ?></td>
+				<td><?php print htmlspecialchars($c['cert_parsed']['subject']['CN'] ?? '/'); ?></td>
+			</tr>
+
+			<?php if (!empty($c['san_list'])): ?>
+			<tr>
+				<td class="text-secondary"><?php print _("SANs"); ?></td>
+				<td><?php print implode("<br>", array_map('htmlspecialchars', $c['san_list'])); ?></td>
+			</tr>
+			<?php endif; ?>
+
+			<tr>
+				<td class="text-secondary"><?php print _("Issuer"); ?></td>
+				<td><?php print htmlspecialchars($c['cert_parsed']['issuer']['CN'] ?? ($c['cert_parsed']['issuer']['O'] ?? '/')); ?></td>
+			</tr>
+
+			<tr>
+				<td class="text-secondary"><?php print _("Valid from/to"); ?></td>
+				<td>
+					<span class='text-secondary'>
+						<?php print isset($c['cert_parsed']['custom_validFrom']) ? htmlspecialchars($c['cert_parsed']['custom_validFrom']) : date("Y-m-d H:i:s", $c['cert_parsed']['validFrom_time_t']); ?>
+					</span> -
+					<span class='text-<?php print $c['days_class']; ?>'>
+						<?php print isset($c['cert_parsed']['custom_validTo']) ? htmlspecialchars($c['cert_parsed']['custom_validTo']) : date("Y-m-d H:i:s", $c['cert_parsed']['validTo_time_t']); ?>
+					</span>
+					<span class='badge text-<?php print $c['days_class']; ?>'><?php print htmlspecialchars($c['days_valid']); ?> <?php print _("Days"); ?></span>
+				</td>
+			</tr>
+
+		</table>
 		<?php endif; ?>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Issuer"); ?></td>
-			<td><?php print htmlspecialchars($cert_parsed['issuer']['CN'] ?? ($cert_parsed['issuer']['O'] ?? '/')); ?></td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Valid from"); ?></td>
-			<td><span class='text-secondary'><?php print isset($cert_parsed['custom_validFrom']) ? htmlspecialchars($cert_parsed['custom_validFrom']) : date("Y-m-d H:i:s", $cert_parsed['validFrom_time_t']); ?></span></td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Valid to"); ?></td>
-			<td class='text-<?php print $days_class; ?>'><?php print isset($cert_parsed['custom_validTo']) ? htmlspecialchars($cert_parsed['custom_validTo']) : date("Y-m-d H:i:s", $cert_parsed['validTo_time_t']); ?></td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Days remaining"); ?></td>
-			<td><span class='text-<?php print $days_class; ?>'><?php print htmlspecialchars($days_valid); ?></span></td>
-		</tr>
-
-	</table>
-	<?php endif; ?>
+		</div>
 	</div>
-</div>
-
-<?php if (!empty($host->c_id_old) && $cert_old_parsed !== null): ?>
-<div class='card' style='margin-top:10px;'>
-	<div class='card-header text-secondary'><?php print $icon_cert; ?> <?php print _("Previous certificate"); ?></div>
-	<div>
-	<table class='table table-borderless table-md table-hover table-zones-details'>
-
-		<tr>
-			<td class="text-secondary" style='min-width:160px;width:180px;'><?php print _("Status"); ?></td>
-			<td>
-				<?php print ($cert_old_status['text']); ?>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Serial"); ?></td>
-			<td>
-				<a class='btn btn-sm' target="_blank"  href='/<?php print $_params['tenant']; ?>/certificates/<?php print $zone->name; ?>/<?php print $cert_old_parsed['serialNumber']; ?>/'>
-					<?php print htmlspecialchars(chunk_split($cert_old_parsed['serialNumber'])); ?>
-				</a>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Common name"); ?></td>
-			<td><?php print htmlspecialchars($cert_old_parsed['subject']['CN'] ?? '/'); ?></td>
-		</tr>
-
-		<?php if (!empty($cert_old_san_list)): ?>
-		<tr>
-			<td class="text-secondary"><?php print _("SANs"); ?></td>
-			<td><?php print implode("<br>", array_map('htmlspecialchars', $cert_old_san_list)); ?></td>
-		</tr>
-		<?php endif; ?>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Issuer"); ?></td>
-			<td><?php print htmlspecialchars($cert_old_parsed['issuer']['CN'] ?? ($cert_old_parsed['issuer']['O'] ?? '/')); ?></td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Valid from"); ?></td>
-			<td><span class='text-secondary'><?php print isset($cert_old_parsed['custom_validFrom']) ? htmlspecialchars($cert_old_parsed['custom_validFrom']) : date("Y-m-d H:i:s", $cert_old_parsed['validFrom_time_t']); ?></span></td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Valid to"); ?></td>
-			<td class='text-<?php print $cert_old_days_class; ?>'><?php print isset($cert_old_parsed['custom_validTo']) ? htmlspecialchars($cert_old_parsed['custom_validTo']) : date("Y-m-d H:i:s", $cert_old_parsed['validTo_time_t']); ?></td>
-		</tr>
-
-		<tr>
-			<td class="text-secondary"><?php print _("Days remaining"); ?></td>
-			<td><span class='text-<?php print $cert_old_days_class; ?>'><?php print htmlspecialchars($cert_old_days_valid); ?></span></td>
-		</tr>
-
-	</table>
-	</div>
-</div>
-<?php endif; ?>
+	<?php endforeach; ?>
--- route/zones/zone/host/index.php
+++ route/zones/zone/host/index.php
@@ -18,110 +18,109 @@ if ($zone === null) {
 }
 else {
 
-# fetch host with current certificate
-$host = $Zones->get_host_with_certificate($hostname, $zone->id);
+	# fetch host with current certificate
+	$host = $Zones->get_host_with_certificate($hostname, $zone->id);
 
-# invalid host or access denied
-if ($host === null) {
-	print '<div class="page-header">';
-	print '	<h2 class="page-title">'._("Invalid host").'</h2><hr>';
-	print '</div>';
-	$Result->show("danger", _("Host does not exist in this zone."), false);
-}
-elseif ($user->admin != "1" && $host->t_id != $user->t_id) {
-	print '<div class="page-header"><h2 class="page-title">'._("Access denied").'</h2><hr></div>';
-	$Result->show("danger", _("Access denied."), false);
-}
-else {
-
-# ---------- current certificate ----------
-$cert_parsed = $Certificates->parse_cert($host->certificate);
-$status      = $Certificates->get_status($cert_parsed, true, true, $hostname);
-$textclass   = $Certificates->get_status_color($status['code']);
-
-$days_valid = isset($cert_parsed['custom_validDays']) ? $cert_parsed['custom_validDays'] : "/";
-if (is_numeric($days_valid)) {
-	if     ($days_valid < 0)              { $days_class = "red"; }
-	elseif ($days_valid <= $expired_days) { $days_class = "orange"; }
-	else                                  { $days_class = "green"; }
-} else {
-	$days_class = "secondary";
-}
-
-$san_list = [];
-if (isset($cert_parsed['extensions']['subjectAltName'])) {
-	foreach (explode(",", $cert_parsed['extensions']['subjectAltName']) as $san) {
-		$san = trim($san);
-		if (strlen($san) > 0) { $san_list[] = $san; }
+	# invalid host or access denied
+	if ($host === null) {
+		print '<div class="page-header">';
+		print '	<h2 class="page-title">'._("Invalid host").'</h2><hr>';
+		print '</div>';
+		$Result->show("danger", _("Host does not exist in this zone."), false);
 	}
-}
+	elseif ($user->admin != "1" && $host->t_id != $user->t_id) {
+		print '<div class="page-header"><h2 class="page-title">'._("Access denied").'</h2><hr></div>';
+		$Result->show("danger", _("Access denied."), false);
+	}
+	else {
 
-# ---------- previous certificate ----------
-$cert_old_parsed     = null;
-$cert_old_status     = null;
-$cert_old_textclass  = 'secondary';
-$cert_old_days_valid = "/";
-$cert_old_days_class = "secondary";
-$cert_old_san_list   = [];
+		# ---------- current certificate ----------
+		$cert_parsed = $Certificates->parse_cert($host->certificate);
+		$status      = $Certificates->get_status($cert_parsed, true, true, $hostname);
+		$textclass   = $Certificates->get_status_color($status['code']);
 
-if (!empty($host->c_id_old)) {
-	$cert_old = $Zones->get_host_old_certificate($host->c_id_old);
-	if ($cert_old) {
-		$cert_old_parsed = $Certificates->parse_cert($cert_old->certificate);
-		$cert_old_status = $Certificates->get_status($cert_old_parsed, true, false, "");
-
-		$cert_old_textclass = $Certificates->get_status_color($cert_old_status['code']);
-
-		$cert_old_days_valid = isset($cert_old_parsed['custom_validDays']) ? $cert_old_parsed['custom_validDays'] : "/";
-		if (is_numeric($cert_old_days_valid)) {
-			if     ($cert_old_days_valid < 0)              { $cert_old_days_class = "red"; }
-			elseif ($cert_old_days_valid <= $expired_days) { $cert_old_days_class = "orange"; }
-			else                                           { $cert_old_days_class = "green"; }
+		$days_valid = isset($cert_parsed['custom_validDays']) ? $cert_parsed['custom_validDays'] : "/";
+		if (is_numeric($days_valid)) {
+			if     ($days_valid < 0)              { $days_class = "red"; }
+			elseif ($days_valid <= $expired_days) { $days_class = "orange"; }
+			else                                  { $days_class = "green"; }
+		} else {
+			$days_class = "secondary";
 		}
 
-		if (isset($cert_old_parsed['extensions']['subjectAltName'])) {
-			foreach (explode(",", $cert_old_parsed['extensions']['subjectAltName']) as $san) {
+		$san_list = [];
+		if (isset($cert_parsed['extensions']['subjectAltName'])) {
+			foreach (explode(",", $cert_parsed['extensions']['subjectAltName']) as $san) {
 				$san = trim($san);
-				if (strlen($san) > 0) { $cert_old_san_list[] = $san; }
+				if (strlen($san) > 0) { $san_list[] = $san; }
 			}
 		}
-	}
-}
 
-# ---------- misc ----------
-$last_check_formatted  = $host->last_check  === NULL ? "/" : date("Y-m-d H:i", strtotime($host->last_check));
-$last_change_formatted = $host->last_change === NULL ? "/" : date("Y-m-d H:i", strtotime($host->last_change));
-$all_port_groups       = $SSL->get_all_port_groups();
-$port_group_name       = isset($all_port_groups[$host->t_id][$host->pg_id]['name']) ? $all_port_groups[$host->t_id][$host->pg_id]['name'] : "/";
-$recipients            = array_filter(explode(";", $host->h_recipients));
-$tenant_recipients     = array_filter(array_map('trim', explode(";", str_replace(",", ";", $host->tenant_recipients))));
+		# ---------- previous certificate ----------
+		$cert_old_parsed     = null;
+		$cert_old_status     = null;
+		$cert_old_textclass  = 'secondary';
+		$cert_old_days_valid = "/";
+		$cert_old_days_class = "secondary";
+		$cert_old_san_list   = [];
 
-$icon_host = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-server"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" /><path d="M3 12m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" /><path d="M7 8l0 .01" /><path d="M7 16l0 .01" /></svg>';
-?>
+		if (!empty($host->c_id_old)) {
+			$cert_old = $Zones->get_host_old_certificate($host->c_id_old);
+			if ($cert_old) {
+				$cert_old_parsed = $Certificates->parse_cert($cert_old->certificate);
+				$cert_old_status = $Certificates->get_status($cert_old_parsed, true, false, "");
 
-<div class='page-header'>
-	<h2 class='page-title'><?php print $url_items["zones"]['icon']." "._("Host details"); ?> [<?php print htmlspecialchars($hostname); ?>]</h2>
-	<hr>
-</div>
+				$cert_old_textclass = $Certificates->get_status_color($cert_old_status['code']);
 
-<div>
-	<a href="/<?php print $_params['tenant']; ?>/zones/<?php print htmlspecialchars($zone->name); ?>/" onClick="history.go(-1); return false;" class="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg> <?php print _("Back"); ?></a>
-</div><br><br>
+				$cert_old_days_valid = isset($cert_old_parsed['custom_validDays']) ? $cert_old_parsed['custom_validDays'] : "/";
+				if (is_numeric($cert_old_days_valid)) {
+					if     ($cert_old_days_valid < 0)              { $cert_old_days_class = "red"; }
+					elseif ($cert_old_days_valid <= $expired_days) { $cert_old_days_class = "orange"; }
+					else                                           { $cert_old_days_class = "green"; }
+				}
 
-<div class='row'>
-	<div class='col-xs-12 col-sm-12 col-md-6' style='margin-top:10px;'>
-		<?php include("host-details.php"); ?>
-	</div>
-	<div class='col-xs-12 col-sm-12 col-md-6' style='margin-top:10px;'>
-		<?php include("host-certificate.php"); ?>
-	</div>
-</div>
+				if (isset($cert_old_parsed['extensions']['subjectAltName'])) {
+					foreach (explode(",", $cert_old_parsed['extensions']['subjectAltName']) as $san) {
+						$san = trim($san);
+						if (strlen($san) > 0) { $cert_old_san_list[] = $san; }
+					}
+				}
+			}
+		}
 
-<?php include("host-recipients.php"); ?>
+		# ---------- misc ----------
+		$last_check_formatted  = $host->last_check  === NULL ? "/" : date("Y-m-d H:i", strtotime($host->last_check));
+		$last_change_formatted = $host->last_change === NULL ? "/" : date("Y-m-d H:i", strtotime($host->last_change));
+		$all_port_groups       = $SSL->get_all_port_groups();
+		$port_group_name       = isset($all_port_groups[$host->t_id][$host->pg_id]['name']) ? $all_port_groups[$host->t_id][$host->pg_id]['name'] : "/";
+		$recipients            = array_filter(explode(";", $host->h_recipients));
+		$tenant_recipients     = array_filter(array_map('trim', explode(";", str_replace(",", ";", $host->tenant_recipients))));
 
-<?php include("host-logs.php"); ?>
+		$icon_host = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-server"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" /><path d="M3 12m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" /><path d="M7 8l0 .01" /><path d="M7 16l0 .01" /></svg>';
+		?>
 
-<?php
-} // end else (host valid)
-} // end else (zone valid)
-?>
+		<div class='page-header'>
+			<h2 class='page-title'><?php print $url_items["zones"]['icon']." "._("Host details"); ?> [<?php print htmlspecialchars($hostname); ?>]</h2>
+			<hr>
+		</div>
+
+		<div>
+			<a href="/<?php print $_params['tenant']; ?>/zones/<?php print htmlspecialchars($zone->name); ?>/" onClick="history.go(-1); return false;" class="btn btn-sm btn-outline-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 6l-6 6l6 6" /></svg> <?php print _("Back"); ?></a>
+		</div><br><br>
+
+		<div class='row'>
+			<div class='col-xs-12 col-sm-12 col-md-6' style='margin-top:10px;'>
+				<?php include("host-details.php"); ?>
+			</div>
+			<div class='col-xs-12 col-sm-12 col-md-6' style='margin-top:10px;'>
+				<?php include("host-certificate.php"); ?>
+			</div>
+		</div>
+
+		<?php include("host-recipients.php"); ?>
+
+		<?php include("host-logs.php"); ?>
+
+		<?php
+		} // end else (host valid)
+	} // end else (zone valid)
