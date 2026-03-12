6f60bec0c71c8f4d51f79c2c7b88fc24d027e5f2
Miha Petkovšek <miha.petkovsek@telemach.si>
2026-03-12 12:37:00 +0100

    Added link to agents

--- route/scanning/agents/index.php
+++ route/scanning/agents/index.php
@@ -44,7 +44,16 @@ print '<a href="/" onClick="history.go(-1); return false;" class="btn btn-sm btn
 if($user->admin=="0") {
 print '<a href="/route/modals/agents/edit.php?action=add&tenant='.$user->t_id.'" data-bs-toggle="modal" class="btn btn-sm btn-outline-success"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-2"><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg> '._("New agent").'</a>';
 }
-print '</div><br><br>';
+print '</div>';
+
+print '<div style="float:right">';
+print '<a href="https://github.com/phpipam/php-ssl-agent" class="btn btn-5 align-right" target="_blank" rel="noreferrer">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-2">
+    <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+  </svg>
+  Agents source code
+</a>';
+print '</div>';
 
 # text
 print "<p class='text-secondary'>"._('List of all agents').".</p>";
