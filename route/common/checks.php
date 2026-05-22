b98ba6fb8e133733b9d330993859e2f23f1f285e
Miha Petkovšek <miha.petkovsek@telemach.si>
2026-05-22 09:00:06 +0200

    Bugfixes

--- route/common/checks.php
+++ route/common/checks.php
@@ -2,10 +2,9 @@
 
 // Submodule presence checks — shown to all logged-in users
 $submodules = [
-    'Net_DNS2'  => ['path' => __DIR__ . '/../../functions/assets/Net_DNS2/Net/DNS2.php',     'url' => 'https://github.com/mikepultz/netdns2'],
-    // 'Net_DNS2'  => ['path' => __DIR__ . '/../../functions/assets/Net_DNS2/Net/DNS2.php',     'url' => 'https://github.com/mikepultz/netdns2'],
+    'Net_DNS2'  => ['path' => __DIR__ . '/../../functions/assets/Net_DNS2/Net/DNS2.php',      'url' => 'https://github.com/mikepultz/netdns2'],
     'PHPMailer' => ['path' => __DIR__ . '/../../functions/assets/PHPMailer/src/PHPMailer.php','url' => 'https://github.com/PHPMailer/PHPMailer'],
-    'testssl.sh' => ['path' => __DIR__ . '/../../functions/testSSL/testssl.sh',              'url' => 'https://github.com/testssl/testssl.sh'],
+    'testssl.sh' => ['path' => __DIR__ . '/../../functions/testSSL/testssl.sh',               'url' => 'https://github.com/testssl/testssl.sh'],
 ];
 $missing_submodules = [];
 foreach ($submodules as $name => $info) {
