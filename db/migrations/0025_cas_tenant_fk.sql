6455d0a02aab1f6654195f5a24ee52f14d582458
Miha Petkovšek <miha.petkovsek@telemach.si>
2026-05-27 10:49:57 +0200

    Migration fixes

--- db/migrations/0025_cas_tenant_fk.sql
+++ db/migrations/0025_cas_tenant_fk.sql
@@ -1,1 +1,4 @@
+-- tenants.id is int(11) unsigned; cas.t_id must match for the FK to form
+ALTER TABLE `cas` MODIFY COLUMN `t_id` int(11) unsigned NOT NULL;
+
 ALTER TABLE `cas` ADD CONSTRAINT `cas_tenant_fk` FOREIGN KEY (`t_id`) REFERENCES `tenants` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
